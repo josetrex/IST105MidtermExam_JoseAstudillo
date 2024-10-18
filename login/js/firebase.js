9b6159cd6980a7808581107eaa7cf25d55da4305
Jose Luis Astudillo Idrovo <josetrex34@gmail.com>
2024-10-17 20:42:02 -0700

    a lo hecho pecho

---
+++ login/js/firebase.js
@@ -0,0 +1,15 @@
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBNeopl6vnEhtowpi8UboKzcwpIcv892r4",
+  authDomain: "ist105midtermexamjoseastudillo.firebaseapp.com",
+  projectId: "ist105midtermexamjoseastudillo",
+  storageBucket: "ist105midtermexamjoseastudillo.appspot.com",
+  messagingSenderId: "89429861457",
+  appId: "1:89429861457:web:d8dbfd396c2f069eefc470",
+  measurementId: "G-39CT1G6SQT"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
